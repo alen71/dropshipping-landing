50f69fda51237345e8aaf0eb5f4839d138b275b6
alen71 <alen_babanovic123@hotmail.com>
2023-06-19 22:41:22 +0200

    mobile

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -73,7 +73,7 @@ const Hero = () => {
               whileInView={{ scale: 1 }}
               transition={{ type: 'spring', stiffness: 90, delay: 0.5 }}
               viewport={{ once: true }}
-              className="rounded-full border-2 border-white grid place-items-center w-14 h-14"
+              className="rounded-full border-2 border-white grid place-items-center w-12 lg:w-14 h-12 lg:h-14"
             >
               <SignInIcon />
             </motion.div>
